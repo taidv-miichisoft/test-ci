df04b288bd89c17f03b1b0737c05c916a1946351
khiemnt <khiemnt@miichisoft.net>
2024-01-25 11:01:49 +0700

    test1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <p>Hello World! This is the Home page</p>
+      <p>Hello World World! This is the Home page</p>
       <p>
         Visit the <Link href="/about">About</Link> page.
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
